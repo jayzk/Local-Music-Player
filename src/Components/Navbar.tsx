9a8e84d425d4b6276094938f31f9fe555132bcbf
Jayden <jaydenzkyeo@gmail.com>
2024-06-17 01:48:29 -0600

    Redesigning the Navbar

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,12 +1,39 @@
 import { NavLink } from "react-router-dom";
+import { HomeIcon, MusicalNoteIcon, PlayIcon } from "@heroicons/react/20/solid";
+import logo from "../../public/assets/Logo.png"
 
 export default function Navbar() {
+  const getIcon = (title: string) => {
+    switch (title) {
+      case 'Home':
+        return <HomeIcon className="size-5 mx-2" />;
+      case 'Music':
+        return <MusicalNoteIcon className="size-5 mx-2" />;
+      case 'Player':
+        return <PlayIcon className="size-5 mx-2" />;
+      default:
+        return null;
+    }
+  };
+
   return (
-      <nav className="bg-gray-800 flex flex-col h-screen w-1/6 items-center px-4 text-white">
-          <h1>TESTING</h1>
-          <NavLink to="/">Home</NavLink>
-          <NavLink to="/MusicExplorer">Music</NavLink>
-          <NavLink to="/Player">Player</NavLink>
+      <nav className="bg-slate-900 flex flex-col px-2 h-screen w-1/6 items-center drop-shadow-lg">
+          <div className="my-3 size-24">
+            <img src={logo} />
+          </div>
+          {
+            [
+              ['Home', '/'],
+              ['Music', '/MusicExplorer'],
+              ['Player', '/Player'],
+            ].map(([title, url]) => (
+              <NavLink key={title} to={url} className="w-full rounded-lg px-3 py-3 text-white font-medium hover:bg-slate-100 hover:text-slate-900">
+                <div className="flex flex-row w-full items-center">
+                  {getIcon(title)} {title}
+                </div>
+              </NavLink>
+            ))
+          }
       </nav>
   );
 }
