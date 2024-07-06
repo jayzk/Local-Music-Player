e10a8808e5430e6d93b7f0336bd5036b0dca4dca
Jayden <jaydenzkyeo@gmail.com>
2024-07-05 23:15:58 -0600

    Added conditional rendering based on if the directory has been initialized or not

--- src/Components/DisplayHome.tsx
+++ src/Components/DisplayHome.tsx
@@ -1,17 +1,40 @@
-import { FolderIcon } from "@heroicons/react/24/outline";
-import React, { useEffect } from "react";
+import { FolderIcon, QuestionMarkCircleIcon } from "@heroicons/react/24/outline";
+import React, { useEffect, useState } from "react";
 
 type DisplayHomeProps = {
   settingsData: any;
 };
 
 export default function DisplayHome({ settingsData }: DisplayHomeProps) {
+  const [isConfirmed, setIsConfirmed] = useState(false);
+  const [isInitialized, setIsInitialized] = useState(false);
+
   //re-render when settingsData updates
   useEffect(() => {
+    //check if an sqlite file exists in the new directory
+    const checkSqliteFile = async () => {
+      if(settingsData) {
+        const sqliteFileExists = await window.ipcRenderer.invoke('sqlite-file-exists');
+        console.log("Does sqlite file exist: ", sqliteFileExists);
+        setIsInitialized(sqliteFileExists);
+        setIsConfirmed(false); //reset confirm state
+      }
+    }
+
+    //display new settings data
     if (settingsData) {
       console.log("DisplayHome -> Passed settings: ", settingsData);
     }
+
+    checkSqliteFile();
   }, [settingsData]);
+
+  const handleConfirm = async () => {  
+    setIsConfirmed(!isConfirmed);
+    console.log("Confirm btn pressed, confirm = ", isConfirmed);
+
+    await window.ipcRenderer.invoke('create-database');
+  }
 
   if (!settingsData || settingsData?.selectedDir === "") {
     //No directory/folder is selected
@@ -23,6 +46,21 @@ export default function DisplayHome({ settingsData }: DisplayHomeProps) {
     );
   } else {
 
-    return <div>DisplayHome</div>;
+    return (
+      <div className="h-full">
+        {isConfirmed || isInitialized ? (
+          <p>test</p>
+        ) : (
+          <div className="flex h-full flex-col items-center justify-center text-gray-400">
+            <QuestionMarkCircleIcon className="size-48 animate-bounce" />
+            <p>Confirm and initialize this directory/folder as your music directory?</p>
+            <p>(A database file will be added in this directory/folder)</p>
+            <button className="rounded-lg p-2 m-2 bg-indigo-600 text-white hover:bg-indigo-500" onClick={handleConfirm}>
+              Confirm
+            </button>
+          </div>
+        )}
+      </div>
+    );
   }
 }
