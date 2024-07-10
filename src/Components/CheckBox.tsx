d9cff103c1d789dd28e72887c5d4a956017ebc98
Jayden <jaydenzkyeo@gmail.com>
2024-07-10 01:51:47 -0600

    Added a checkbox for downloading thumbnails
    - Added a bouncing icon to the add music page

---
+++ src/Components/CheckBox.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { CheckIcon } from "@heroicons/react/20/solid";
+
+type CheckboxProps = {
+  label: string;
+  checked: boolean;
+  onChange: (checked: boolean) => void;
+};
+
+export default function CheckBox({ label, checked, onChange }: CheckboxProps) {
+  return (
+    <label className="relative flex items-center">
+      <input
+        type="checkbox"
+        className="delay-50 form-checkbox peer mx-2 h-5 w-5 appearance-none rounded-md border-2 border-slate-400 bg-slate-400 transition checked:border-indigo-500 checked:bg-indigo-500 hover:border-white"
+        checked={checked}
+        onChange={(e) => onChange(e.target.checked)}
+      />
+      <span className="text-white">{label}</span>
+      <CheckIcon className="invisible absolute left-2 size-5 text-white peer-checked:visible" />
+    </label>
+  );
+}
--- src/pages/AddMusicYT.tsx
+++ src/pages/AddMusicYT.tsx
@@ -1,5 +1,6 @@
-import React, { useEffect, useState } from 'react'
-import { FolderIcon } from '@heroicons/react/20/solid'
+import React, { useEffect, useState } from "react";
+import { FolderIcon, FolderArrowDownIcon } from "@heroicons/react/20/solid";
+import CheckBox from "../Components/CheckBox";
 
 async function fetchSettings() {
   try {
@@ -14,10 +15,13 @@ async function fetchSettings() {
 export default function AddMusicYT() {
   const [settingsData, setSettingsData] = useState<any | null>(null);
 
+  const [checkedItems, setCheckedItems] = useState<{ [key: string]: boolean }>({
+    thumbnailChecked: false,
+  });
+
   const handleOpenDirDialog = async () => {
     const filePaths = await window.ipcRenderer.invoke("open-dir-dialog");
     if (filePaths.length > 0) {
-
       //get new directory
       console.log("New directory selected: ", filePaths[0]);
       await window.ipcRenderer.invoke(
@@ -31,6 +35,13 @@ export default function AddMusicYT() {
       console.log("New settings: ", newSettingsData);
       setSettingsData(newSettingsData);
     }
+  };
+
+  const handleCheckboxChange = (item: string, checked: boolean) => {
+    setCheckedItems((prev) => ({
+      ...prev,
+      [item]: checked,
+    }));
   };
 
   //run on mount
@@ -49,48 +60,64 @@ export default function AddMusicYT() {
       console.log("AddMusicYT -> Settings Data: ", settingsData);
     }
   }, [settingsData]);
-  
+
+  //log checkbox info
+  useEffect(() => {
+    if (checkedItems) {
+      console.log("AddMusicYT -> CheckBox data: ", checkedItems);
+    }
+  }, [checkedItems]);
+
   return (
-    <div className='h-full'>
-      <div className='h-[15%] space-y-2 py-2 border-b-2 border-slate-700'>
-        <div className='flex w-full text-white justify-center items-center'>
-          <FolderIcon className='size-10 mr-2'/>
+    <div className="h-full">
+      <div className="h-[15%] space-y-2 border-b-2 border-slate-700 py-2">
+        <div className="flex w-full items-center justify-center text-white">
+          <FolderIcon className="mr-2 size-10" />
           Current Music Folder
         </div>
-        <div className='flex w-full text-white justify-center items-center space-x-2'>
+        <div className="flex w-full items-center justify-center space-x-2 text-white">
           <input
-              className="truncate bg-slate-600 p-1 rounded-lg"
-              placeholder={settingsData?.selectedDir}
-              size={50}
-              disabled
-            />
-            <button
-              onClick={handleOpenDirDialog}
-              className="rounded-lg bg-indigo-600 p-1 px-2 text-white hover:bg-indigo-500"
-            >
-              Change...
-            </button>
+            className="truncate rounded-lg bg-slate-600 p-1"
+            placeholder={settingsData?.selectedDir}
+            size={50}
+            disabled
+          />
+          <button
+            onClick={handleOpenDirDialog}
+            className="rounded-lg bg-indigo-600 p-1 px-2 text-white hover:bg-indigo-500"
+          >
+            Change...
+          </button>
         </div>
       </div>
-      <div className='h-[85%] flex flex-col justify-center items-center space-y-2'>
-        <div className='text-lg text-white flex flex-row'>
-          <p className='font-bold underline underline-offset-1 mx-2'>Insert Youtube URL here!</p> 
+      <div className="flex h-[85%] flex-col items-center justify-center space-y-2">
+        <FolderArrowDownIcon className="size-48 text-gray-400 animate-bounce" />
+        <div className="flex flex-row text-lg text-white">
+          <p className="mx-2 font-bold underline underline-offset-1">
+            Insert Youtube URL here!
+          </p>
           Downloaded contents will be stored in your music folder
         </div>
-        <div className='flex flex-row space-x-2'>
+        <div className="flex flex-row space-x-2">
           <input
-              className="truncate transition duration-200 bg-slate-600 p-1 rounded-lg text-lg text-white outline-none border-2 border-slate-600 focus:border-white"
-              placeholder="Input Youtube URL here..."
-              size={50}
-            />
-            <button
-              className="rounded-lg bg-indigo-600 p-1 px-2 text-white hover:bg-indigo-500"
-            >
-              Download
-            </button>
+            className="truncate rounded-lg border-2 border-slate-600 bg-slate-600 p-1 text-lg text-white outline-none transition duration-200 focus:border-white"
+            placeholder="Input Youtube URL here..."
+            size={50}
+          />
+          <button className="rounded-lg bg-indigo-600 p-1 px-2 text-white hover:bg-indigo-500">
+            Download
+          </button>
         </div>
-        
+        <div>
+          <CheckBox
+            label="Download Thumbnail"
+            checked={checkedItems.item1}
+            onChange={(checked) =>
+              handleCheckboxChange("thumbnailChecked", checked)
+            }
+          />
+        </div>
       </div>
     </div>
-  )
+  );
 }
