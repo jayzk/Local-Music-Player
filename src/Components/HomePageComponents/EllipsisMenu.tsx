d31d3d48d08381f832c1e75fdce8ce3c98fc6bce
Jayden <jaydenzkyeo@gmail.com>
2024-08-28 16:45:53 -0600

    Add additional gui options to ellipsis menu

--- src/Components/HomePageComponents/EllipsisMenu.tsx
+++ src/Components/HomePageComponents/EllipsisMenu.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { TrashIcon } from "@heroicons/react/20/solid";
+import { TrashIcon, MusicalNoteIcon, UserIcon, PhotoIcon } from "@heroicons/react/20/solid";
 import { songType } from "../../../public/types";
 import DialogDeleteSong from "./Dialogs/DialogDeleteSong";
 
@@ -20,15 +20,39 @@ export default function EllipsisMenu({ song, onClick, componentRef }: EllipsisMe
   return (
     <>
       <div
-        className="flex items-center justify-center rounded-lg bg-slate-900 p-2"
+        className="flex flex-col justify-center rounded-lg bg-slate-900 p-2"
         onClick={onClick}
         ref={componentRef}
       >
         <button
           className="p-1 px-2 text-sm text-white hover:bg-slate-600"
+        >
+          <div className="flex items-center">
+            <MusicalNoteIcon className="mr-2 size-4" />
+            Change song title
+          </div>
+        </button>
+        <button
+          className="p-1 px-2 text-sm text-white hover:bg-slate-600"
+        >
+          <div className="flex items-center">
+            <UserIcon className="mr-2 size-4" />
+            Change artist
+          </div>
+        </button>
+        <button
+          className="p-1 px-2 text-sm text-white hover:bg-slate-600"
+        >
+          <div className="flex items-center">
+            <PhotoIcon className="mr-2 size-4" />
+            Change Thumbnail
+          </div>
+        </button>
+        <button
+          className="p-1 px-2 text-sm text-white hover:bg-slate-600"
           onClick={handleRemove}
         >
-          <div className="flex items-center justify-center">
+          <div className="flex items-center">
             <TrashIcon className="mr-2 size-4" />
             Remove
           </div>
